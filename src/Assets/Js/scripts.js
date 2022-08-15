56187195ca9d09f6b3a4cd1ccdabbe642a6d3faa
Dchole <derekoware47@gmail.com>
2022-08-15 15:19:37 +0000

    feat: navbar open and close transitions

--- src/Assets/Js/scripts.js
+++ src/Assets/Js/scripts.js
@@ -10,13 +10,47 @@ $(document).ready(function() {
 });
 
 
+const hamburger = document.querySelector("#hamburger");
+/** @type {HTMLDivElement|null} */
+const navFlex = document.querySelector(".nav-flex");
 
-function hamb() {
-    
-    const hamburger = document.querySelector('#hamburger');
-    const navlinks = document.querySelector('.nav-link');
-    navlinks.style.display="none";
+const mobileNavState = {
+    hidden:undefined,
+    height:undefined
 }
+
+hamburger?.addEventListener("click", () => {
+    if (!navFlex?.style.height || navFlex.style.height === "0px") {
+        navFlex.hidden = false;
+        navFlex.style.height = "250px";
+    } else {
+        navFlex.style.height = "0";
+        navFlex.hidden = true;
+    }
+
+    mobileNavState.hidden = navFlex?.hidden
+    mobileNavState.height = navFlex?.style.height
+});
+
+window.addEventListener('resize', () => {
+    const {matches} = window.matchMedia('only screen and (max-width: 600px)')
+    
+    if(!matches) {
+        console.log('on desktop')
+        navFlex.style.height = "fit-content";
+        navFlex.hidden = false;
+    } else {
+        navFlex.style.height = mobileNavState.height;
+        navFlex.hidden = mobileNavState.hidden
+    }
+})
+
+// function hamb() {
+    
+//     const hamburger = document.querySelector('#hamburger');
+//     const navlinks = document.querySelector('.nav-link');
+//     navlinks.style.display="none";
+// }
 
 // var hamb = document.querySelector('#hamburger');
 // var navlinks = document.querySelector('.nav-link');
